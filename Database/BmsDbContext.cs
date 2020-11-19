c3cfab52441799b59acb5106b1e141237c24c81d
JSauceda10 <j.sauceda10@yahoo.com>
2020-11-18 21:02:12 -0600

    Files added to create models for database

---
+++ Database/BmsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BuildingMaterialsEStore.Models.Product;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildingMaterialsEStore.Database
+{
+    public class BmsDbContext:DbContext
+    {
+        public BmsDbContext(DbContextOptions<DbContext> options) : base(options) { }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Models/Product/CategoryStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildingMaterialsEStore.Models.Product
+{
+    public enum CategoryStatus
+    {
+        Active = 0,
+        Inactive = 1,
+    }
+}
